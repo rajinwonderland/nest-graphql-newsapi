9fad39d1f05eee6a3910f6408087f4d15e566860
rajinwonderland <raj@novvuminc.com>
2019-01-24 20:24:16 -0800

    Updates

---
+++ scripts/build_prod.sh
@@ -0,0 +1,4 @@
+export ENVIRONMENT="production"
+export PORT=8080
+yarn build
+yarn start
--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,20 @@ async function bootstrap() {
 	addEnvVars();
 	const app = await NestFactory.create(ApplicationModule);
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(3000);
+	await app.listen(process.env.PORT || 3000);
 	addDx();
 }
 bootstrap();
 
 function addDx() {
 	// @desc a function to add some better developer experience
-	if (process.env.ENVIRONMENT !== 'dev') {
-		return;
+	if (process.env.ENVIRONMENT === 'dev') {
+		openPlayground();
 	}
 	// @def chalk & log instantiation for logging and opening the server url
 	const chalk = require('chalk');
 	const log = console.log;
-
-	// @def opn instantiation for opening the browser for better dX
-	const opn = require('opn');
-
 	log(chalk.cyan('\nStarting the development server...\n'));
-
-	// ! opens the browser
-	opn(`http://localhost:${process.env.PORT || 3000}/graphql`);
 	log(
 		chalk.green(
 			`Serving on ${`http://localhost:${process.env.PORT || 3000}`}/graphql`,
@@ -37,4 +30,11 @@ function addDx() {
 function addEnvVars() {
 	// @def dotenv instantiation for loading .env variables
 	require('dotenv').config();
+}
+
+function openPlayground() {
+	// @def opn instantiation for opening the browser for better dX
+	const opn = require('opn');
+	// ! opens the browser straight to playground
+	opn(`http://localhost:${process.env.PORT || 3000}/graphql`);
 }
